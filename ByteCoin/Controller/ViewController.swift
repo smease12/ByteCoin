3181327a98190b4cab5907d873c8b5ed0d16d625
smease12 <scmease93@gmail.com>
2024-02-10 21:27:43 -0500

    Print currency selected on picker didSelectRow

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -36,7 +36,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print(row)
+        print(coinManager.currencyArray[row])
     }
 }
 
