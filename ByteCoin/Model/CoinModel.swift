55eff911a8ee4d289af0ab9c0fe73016e2125f49
smease12 <scmease93@gmail.com>
2024-02-11 20:57:00 -0500

    Add CoinModel struct

---
+++ ByteCoin/Model/CoinModel.swift
@@ -0,0 +1,16 @@
+//
+//  CoinModel.swift
+//  ByteCoin
+//
+//  Created by Serena  on 2/11/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinModel{
+    
+    let rate: Double
+    let currencyName: String
+    
+}
