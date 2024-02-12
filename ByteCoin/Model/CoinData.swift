da35aa01edd2d81246fe18c5dc17a4a3d903902b
smease12 <scmease93@gmail.com>
2024-02-11 20:38:27 -0500

    Add CoinData struct. Start trying to create performRequest func

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Serena  on 2/11/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable{
+    let rate: Double
+}
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -16,6 +16,44 @@ struct CoinManager {
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getCoinPrice(for currency: String){
+        let urlString = "\(baseURL)/\(currency)?apikey=%205B24E9A7-5CCC-49FC-8401-17A7C776921D"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String){
+        //1. Create a url
         
+        if let url = URL(string: urlString){
+            
+            //2. Create a URLsession
+            let session = URLSession(configuration: .default)
+            
+            //3. Give the session a task
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil{
+                  //  self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                if let safeData = data{
+                    if let exchangeRate = self.parseJSON(exchangeRate: safeData){
+                        //self.delegate?.didUpdateWeather(self, weather: weather)
+                    }
+                }
+            }
+            //4. Start the task
+            task.resume()
+        }
+    }
+    
+    func parseJSON(exchangeRate: Data) -> CoinData?{
+        let decoder = JSONDecoder()
+        do{
+           let decodedData = try decoder.decode(CoinData.self, from: exchangeRate)
+            let rate = decodedData.rate
+            return rate
+        } catch{
+            self.delegate?.didFailWithError(error: error)
+            return nil
+        }
     }
 }
